0e5286b3230a462dcb8ce4c3b136b207e1f91455
Carlos-Marques <carlosmarques.personal@gmail.com>
2021-06-11 15:19:11 +0100

    important dot files

---
+++ .doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elfeed-feeds
+   '("https://fenix.tecnico.ulisboa.pt/disciplinas/IIPEEC64511132646/2020-2021/2-semestre/rss/announcement" "https://fenix.tecnico.ulisboa.pt/disciplinas/GPE1011132646/2020-2021/2-semestre/rss/announcement" "https://fenix.tecnico.ulisboa.pt/disciplinas/POO364511132646/2020-2021/2-semestre/rss/announcement" "https://fenix.tecnico.ulisboa.pt/disciplinas/PEst5146/2020-2021/2-semestre/rss/announcement" "https://fenix.tecnico.ulisboa.pt/disciplinas/CEE364511132646/2020-2021/2-semestre/rss/announcement")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
